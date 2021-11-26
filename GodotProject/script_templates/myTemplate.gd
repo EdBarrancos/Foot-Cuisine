2cd8fb5e9064158b9bc2f9d5257fe068a0512fb8
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-11-26 16:26:02 +0000

    Create Script Template

---
+++ GodotProject/script_templates/myTemplate.gd
@@ -0,0 +1,23 @@
+extends %BASE%
+
+###########
+#VARIABLES#
+###########
+
+
+
+########
+#EVENTS#
+########
+
+func _ready()%VOID_RETURN%:
+	pass # Replace with function body.
+
+
+#func _process(delta%FLOAT_TYPE%)%VOID_RETURN%:
+#   pass
+
+
+##############
+#MISCELANIOUS#
+##############
