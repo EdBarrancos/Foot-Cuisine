81b1f717cfa1ab73fdb2e599f9a863ec307fea1b
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-11-27 15:39:25 +0000

    Player Movement

--- GodotProject/Prototype/Scripts/Player/Storage/PlayerMovement.gd
+++ GodotProject/Prototype/Scripts/Player/Storage/PlayerMovement.gd
@@ -10,6 +10,9 @@ extends Node2D
 
 var player
 
+onready var velocity = Vector2.ZERO
+
+
 ########
 #EVENTS#
 ########
@@ -18,10 +21,24 @@ func Init(player_):
 	player = player_
 
 
-#func _process(delta):
-#   pass
+func _process(delta):
+	GetInput()
+	
+func _physics_process(delta):
+	Move()
+
+func GetInput():
+	if Input.is_action_pressed("LEFT"):
+		velocity = Vector2(-1,0)
+	elif Input.is_action_pressed("RIGHT"):
+		velocity = Vector2(1,0)
+	else:
+		velocity = Vector2.ZERO
 
 
-##############
-#MISCELANIOUS#
-##############
+##########
+#MOVEMENT#
+##########
+
+func Move():
+	player.move_and_slide(velocity*200)
