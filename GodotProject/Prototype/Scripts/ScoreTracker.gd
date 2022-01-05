2fd5465b367eaa20e4150d113799f56db9ec73a0
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2022-01-05 00:31:31 +0000

    Combo Reseter timer

--- GodotProject/Prototype/Scripts/ScoreTracker.gd
+++ GodotProject/Prototype/Scripts/ScoreTracker.gd
@@ -5,6 +5,7 @@ extends Node2D
 ############
 
 onready var scoreSoundDefault = $ScoreDefault
+onready var comboReset = $ComboReset
 
 ###########
 #VARIABLES#
@@ -24,7 +25,6 @@ signal score
 ########
 
 func _ready():
-	load_game()
 	if MaxScore == null:
 		MaxScore = 0
 		
@@ -51,7 +51,6 @@ func FoodScore():
 func EndGame():
 	if score > MaxScore:
 		MaxScore = score
-	save_game()
 	
 #######
 #COMBO#
@@ -70,35 +69,8 @@ func ResetCombo():
 func IncreaseCombo():
 	combo += 1
 	emit_signal("score")
+	comboReset.start()
 
 
-###############
-#LOAD AND SAVE#
-###############
-
-func save_dict():
-	var save_dict = {
-		"MaxScore" : MaxScore,
-	}
-	return save_dict
-
-func save_game():
-	var save_game = File.new()
-	save_game.open("user://FootKitchen.save", File.WRITE)
-	save_game.store_line(to_json(save_dict()))
-	save_game.close()
-	
-func load_game():
-	var save_game = File.new()
-	if not save_game.file_exists("user://FootKitchen.save"):
-		return
-	save_game.open("user://FootKitchen.save", File.READ)
-	
-	var variable_data = parse_json(save_game.get_line())
-	for i in variable_data:
-		set(i, variable_data[i])
-	
-	save_game.close()
-
-
-
+func _on_ComboReset_timeout():
+	ResetCombo()
