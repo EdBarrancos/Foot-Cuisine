7f4a03f063690e878204f9c9c22d38b745eb1533
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-03-09 00:06:58 +0000

    Controller Tutorials

--- GodotProject/Prototype/Scripts/Kitchen/CanvasLayer.gd
+++ GodotProject/Prototype/Scripts/Kitchen/CanvasLayer.gd
@@ -7,6 +7,11 @@ extends CanvasLayer
 onready var move_tutorial = $MoveTutorial
 onready var kick_tutorial = $KickTutorial
 
+export(String) var controller_movement_tutorial
+export(String) var keyboard_movement_tutorial
+export(String) var controller_shooting_tutorial
+export(String) var keyboard_shooting_tutorial
+
 ###########
 #VARIABLES#
 ###########
@@ -19,10 +24,30 @@ func _ready():
 	if Global.played:
 		self.visible = false
 	Global.played = true
+	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
+	if Input.get_connected_joypads().size() >= 1:
+		SetController()
 
 
-#func _process(delta):
-#   pass
+func SetController():
+	move_tutorial.clear()
+	move_tutorial.append_bbcode("[center]" + controller_movement_tutorial + "[/center]")
+	
+	kick_tutorial.clear()
+	kick_tutorial.append_bbcode("[center]" + controller_shooting_tutorial + "[/center]")
+
+func SetKeyboard():
+	move_tutorial.clear()
+	move_tutorial.append_bbcode("[center]" + keyboard_movement_tutorial + "[/center]")
+	
+	kick_tutorial.clear()
+	kick_tutorial.append_bbcode("[center]" + keyboard_shooting_tutorial + "[/center]")
+
+func _on_joy_connection_changed(device_id, connected):
+	if connected:
+		SetController()
+	else:
+		SetKeyboard()
 
 
 ##############
